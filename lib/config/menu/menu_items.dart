d768b5e885f5bc7049fd69d7848c04db4042bda4
mariano <153884968+jimcode1090@users.noreply.github.com>
2025-08-07 16:39:05 -0500

    infinite scroll finish

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -57,4 +57,10 @@ const appMenuItems = <MenuItems>[
     link: '/tutorial',
     icon: Icons.accessible_rounded,
   ),
+  MenuItems(
+    title: 'Infinite Scroll and Pull to Refresh',
+    subtitle: 'Listas infinitas y refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_rounded,
+  ),
 ];
--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,132 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
-
+class InfiniteScrollScreen extends StatefulWidget {
   static const String name = 'infinite_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    // Revisar si esta montado el componente / widget
+    if (!isMounted) return;
+    
+    setState(() {});
+
+    // TODO: mover scroll
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    if(scrollController.position.pixels + 150 <= scrollController.position.maxScrollExtent) return;
+    scrollController.animateTo(
+      scrollController.position.pixels + 120,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
+
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+
+    setState(() {});
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (_, index) {
+              return FadeInImage(
+                placeholder: const AssetImage('assets/images/jar-loading.gif'),
+                image: NetworkImage(
+                  "https://picsum.photos/id/${imagesIds[index]}/500/300",
+                ),
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child:
+            isLoading
+                ? SpinPerfect(
+                  infinite: true,
+                  child: const Icon(Icons.refresh_rounded),
+                )
+                : FadeIn(child: const Icon(Icons.arrow_back_ios_new_rounded)),
+      ),
+    );
   }
 }
